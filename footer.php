450438eccfc5304f261d7755824531f3b95a665b
Joe Davison <joe@warhaggis.com>
2016-03-14 04:10:37 +0000

    license header on php

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+<?php
+/* Woggle 4 Wordpress Remix
+ *
+ * Woggle 4 Wordpress Remix is a free Wordpress theme derived from Woggle 4 Wordpress
+ * developed by hawesWeb Productions available at iscout4wordpress.org.uk
+ *
+ * Woggle 4 Wordpress Remix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Woggle 4 Wordpress Remix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Woggle 4 Wordpress Remix.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+?>
+
 </div>
 
 <?php include(TEMPLATEPATH . '/main.php'); ?>
